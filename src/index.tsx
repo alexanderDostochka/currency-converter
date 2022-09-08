1e1776a770d05f546f21b5a2c48f3de0bc788cd2
alexanderDostochka <chanelsponky@gmail.com>
2022-09-08 23:31:14 +0300

    add sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./styles/index.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById('currency-converter-app') as HTMLElement
